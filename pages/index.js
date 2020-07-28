6e5394fbbe12ea6c7db74db8b093a3aab24dcbdc
Yousaf <waynestudent38@gmail.com>
2020-07-28 07:34:58 -0600

    started integrating google maps API

--- pages/index.js
+++
@@ -1,5 +0,0 @@
-function Home(){
-	return <div>Loading...</div>;
-}
-
-export default Home;
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  }
+};
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
+    ],
+  },
+};
